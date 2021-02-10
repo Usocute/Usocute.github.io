2639b4a580f85bded955e4fe53ba3b45d57479fd
“yourusocute <2737164694@qq.com>
2021-02-10 14:34:55 +0800

    Site updated: 2021-02-10 14:34:40

--- js/main.js
+++ js/main.js
@@ -834,3 +834,18 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
+
+// 可爱的Title
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        document.title = '(つェ⊂) 我藏好了哦~~';
+        clearTimeout(titleTime);
+    } else {
+        document.title = '(*´∇｀*) 被你发现啦~~' + OriginTitle;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
